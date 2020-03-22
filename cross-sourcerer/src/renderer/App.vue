2cf6c6b1a01faf4fd9449d65de2c9b9c28c31c9a
Pierre Hérissé <pierre.herisse@gmail.com>
2020-03-22 01:01:26 +0100

    init electron with vue

---
+++ cross-sourcerer/src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'cross-sourcerer'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
